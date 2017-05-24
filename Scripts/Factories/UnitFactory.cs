9e11beb5f07abbe0f389be1dc8437181b7c1ed4d
samjudge <sam.m.w.judge@googlemail.com>
2017-05-24 21:27:21 +0100

    config loader for unitfactory added

--- Scripts/Factories/UnitFactory.cs
+++ Scripts/Factories/UnitFactory.cs
@@ -1,4 +1,8 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using System.Reflection;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,65 +12,62 @@ public class UnitFactory : MonoBehaviour{
 	public GameObject ChickenEnemy;
 	public GameObject SkeletonEnemy;
 
-	public GameObject CreateUnit(String name, Vector3 position){
-		switch(name){
-			case "Dummy":
-				return Instantiate(DummyEnemy, position, Quaternion.Euler(15,180,0)) as GameObject;
-				break;
-			case "Chicken":
-				GameObject nChickenEnemey = Instantiate(ChickenEnemy, position, Quaternion.Euler(15,180,0)) as GameObject;
-				ChaserEnemy nChickenEnemeyScript = nChickenEnemey.GetComponent<ChaserEnemy>() as ChaserEnemy;
-				nChickenEnemeyScript.DeathAction = delegate(){
-				Player p = nChickenEnemeyScript.GameManager.Player.GetComponent<Player>() as Player;
-					p.Experience += 1;
-					if(p.CanLevelUp()){
-						p.Experience -= p.CalculateRequiredExpForLevelUp();
-						p.level += 1;
-						p.LevelUp();
-					}
-					GameObject ChickenDrop = nChickenEnemeyScript
-						.GameManager
-						.ItemFactory
-						.CreateItem(
-							"Chicken",
-							nChickenEnemeyScript.transform.position
-					);
-					nChickenEnemeyScript.GameManager.level.items.Add(ChickenDrop);
-					nChickenEnemeyScript.GameManager.level.RemoveEnemy(nChickenEnemeyScript);
-				};
-				return nChickenEnemey;
-				break;
-			case "Skeleton":
-				GameObject nSkeletonEnemey = Instantiate(SkeletonEnemy, position, Quaternion.Euler(15,180,0)) as GameObject;
-				ChaserEnemy nSkeletonEnemeyScript = nSkeletonEnemey.GetComponent<ChaserEnemy>() as ChaserEnemy;
-				nSkeletonEnemeyScript.DeathAction = delegate(){
-					Player p = nSkeletonEnemeyScript.GameManager.Player.GetComponent<Player>() as Player;
-					p.Experience += 3;
-					if(p.CanLevelUp()){
-						p.Experience -= p.CalculateRequiredExpForLevelUp();
-						p.level += 1;
-						p.LevelUp();
-					}
-					System.Random r = new System.Random();
-					float rNum = r.Next(0,100);
-					if(rNum < 100f){
-						GameObject SwordDrop = nSkeletonEnemeyScript
-							.GameManager
+	public TextAsset Config;
+
+	public GameObject CreateUnit(string name, Vector3 position){
+		Debug.Log (name + "Enemy");
+		FieldInfo property = this.GetType().GetField(name + "Enemy");
+		Debug.Log (property);
+		GameObject ePrefab = property.GetValue(this) as GameObject;
+		GameObject nEnemey = Instantiate(ePrefab, position, Quaternion.Euler(15,180,0)) as GameObject;
+		XDocument Data = XDocument.Parse(Config.text);
+		IEnumerable<XElement> Units = from unit in Data.Descendants("unit") 
+			where unit.Descendants("name").ElementAt(0).Value as string == name
+			select unit;
+
+		XElement Unit = Units.ElementAt(0);
+
+		String xmlUnitName = Unit.Descendants("name").ElementAt(0).Value as String;
+		String xmlUnitSubtype = Unit.Descendants("subtype").ElementAt(0).Value as String;
+
+		Type EnemyScriptType = Type.GetType(xmlUnitSubtype);
+
+		Enemy nEnemeyScript = nEnemey.GetComponent(EnemyScriptType) as Enemy;
+		IEnumerable<XElement> xmlDeathAction = Unit.Descendants("deathAction");
+		if(xmlDeathAction.Count() != 0){
+			String xmlDeathActionName = xmlDeathAction.Descendants("action").ElementAt(0).Value as String;
+			switch(xmlDeathActionName){
+				case "BasicEnemyDeathAction":
+					nEnemeyScript.DeathAction = delegate(){
+					int xmlDeathActionExp = Int32.Parse(xmlDeathAction.Descendants("experience").ElementAt(0).Value as String);
+						IEnumerable<XElement> xmlDrops = xmlDeathAction.Descendants("drops");
+						Player p = nEnemeyScript.GameManager.Player.GetComponent<Player>() as Player;
+						p.Experience += xmlDeathActionExp;
+						if(p.CanLevelUp()){
+							p.Experience -= p.CalculateRequiredExpForLevelUp();
+							p.level += 1;
+							p.LevelUp();
+						}
+						foreach(XElement drop in xmlDrops){
+							String xmlItemName = drop.Descendants("name").ElementAt(0).Value as String;
+							String xmlItemChance = drop.Descendants("chance").ElementAt(0).Value as String;
+							GameObject Drop = nEnemeyScript
+								.GameManager
 								.ItemFactory
 								.CreateItem(
-									"Sword",
-									nSkeletonEnemeyScript.transform.position
-									);
-						nSkeletonEnemeyScript.GameManager.level.items.Add(SwordDrop);
-					}
-					nSkeletonEnemeyScript.GameManager.level.RemoveEnemy(nSkeletonEnemeyScript);
-				};
-				return nSkeletonEnemey;
-				break;
-			default:
-				return Instantiate(DummyEnemy, position, Quaternion.Euler(15,180,0)) as GameObject;
-				break;
+									xmlItemName,
+									nEnemeyScript.transform.position
+								);
+							nEnemeyScript.GameManager.level.items.Add(Drop);
+						}
+						nEnemeyScript.GameManager.level.RemoveEnemy(nEnemeyScript);
+					};
+					break;
+				default:
+					break;
+			}
 		}
+		return nEnemey;
 	}
 }
 
