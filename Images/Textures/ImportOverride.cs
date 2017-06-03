33ca34a11d158a135dfa8c7980fac96dda876464
samjudge <sam.m.w.judge@googlemail.com>
2017-06-03 23:02:29 +0100

    more stuff

---
+++ Images/Textures/ImportOverride.cs
@@ -0,0 +1,18 @@
+﻿ using UnityEngine;
+ using UnityEditor;
+  
+ public class ImportOverride : AssetPostprocessor
+ {
+     void OnPreprocessTexture()
+     {
+         TextureImporter importer = assetImporter as TextureImporter;
+         Object asset = AssetDatabase.LoadAssetAtPath(importer.assetPath, typeof(Texture2D));
+         if (!asset)
+         {
+             importer.textureType = TextureImporterType.Advanced;
+             importer.npotScale = TextureImporterNPOTScale.None;
+             importer.mipmapEnabled = false;
+             importer.textureFormat = TextureImporterFormat.AutomaticTruecolor;
+         }         
+     }
+ }
